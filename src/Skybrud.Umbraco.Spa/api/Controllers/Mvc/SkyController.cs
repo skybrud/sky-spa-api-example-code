4f47918b0df8eee47c8fe5815b4fe408914b3cb0
René Pjengaard Bank <rpjengaard@skybrud.dk>
2016-06-29 23:52:02 +0200

    Added SkyController and example of StartView Model

---
+++ src/Skybrud.Umbraco.Spa/api/Controllers/Mvc/SkyController.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Mvc;
+using SkybrudSpaExample.Models.Spa;
+using Umbraco.Web.Mvc;
+
+namespace SkybrudSpaExample.Controllers.Mvc {
+    public class SkyController : RenderMvcController {
+        protected ViewResult View(Master model) {
+            return View(null, model);
+        }
+
+        protected ViewResult View(string view, Master model) {
+            return base.View(view, model);
+        }
+    }
+}
---
+++ src/Skybrud.Umbraco.Spa/api/Models/SolutionSpecificModels/StartView.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SkybrudSpaExample.Models.Spa;
+using Umbraco.Core.Models;
+
+namespace SkybrudSpaExample.Models.SolutionSpecificModels
+{
+    public class StartView : Master
+    {
+        #region properties
+
+        #endregion
+
+        #region constructers
+
+        protected StartView(IPublishedContent content)
+            : base(content)
+        {
+        }
+
+        #endregion
+
+        #region static methods
+
+        public static StartView GetFromContent(IPublishedContent content)
+        {
+            return content == null ? null : new StartView(content);
+        }
+
+        #endregion
+    }
+}
